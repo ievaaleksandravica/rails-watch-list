490b4cf45e45a5cbd8c53a4235e0b383d23d1937
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2021-07-20 20:27:12 +0200

    cloudinary photo function

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,10 @@
 class List < ApplicationRecord
+  has_one_attached :photo
+
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
 
   validates :name, uniqueness: true
   validates :name, presence: true
+
 end
